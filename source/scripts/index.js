ebce6513bf17cfec96952103bc9b05b05ba72f20
vmikhas <vmikhas@yandex.ru>
2024-01-25 09:46:17 +0300

    дорабатывает декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+const headerToggle = document.querySelector(".main-header__toggle")
+const headerNav = document.querySelector(".main-nav")
+
+const openNav = () => {
+  headerToggle.classList.toggle("main-header__toggle--open")
+  headerNav.classList.toggle("main-nav--open")
+}
+
+headerToggle.addEventListener("click", openNav);
