a917952c11e1156beac0ce531c8f129a80037fba
vmikhas <vmikhas@yandex.ru>
2024-01-28 20:34:06 +0300

    добавляет карту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,6 @@
 const headerToggle = document.querySelector(".main-header__toggle")
 const headerNav = document.querySelector(".main-nav")
+const mapContainer = document.querySelector(".map__iframe-container")
 
 const openNav = () => {
   headerToggle.classList.toggle("main-header__toggle--open")
@@ -7,3 +8,5 @@ const openNav = () => {
 }
 
 headerToggle.addEventListener("click", openNav);
+
+mapContainer.classList.remove("map__iframe-container--no-js")
