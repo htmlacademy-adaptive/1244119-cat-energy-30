fdbb9ad85eda8d004920091718e644eab95389f9
vmikhas <vmikhas@yandex.ru>
2024-01-30 21:45:38 +0300

    делает правки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,12 +1,12 @@
-const headerToggle = document.querySelector(".main-header__toggle")
-const headerNav = document.querySelector(".main-nav")
-const mapContainer = document.querySelector(".map__iframe-container")
+const headerToggle = document.querySelector('.main-header__toggle')
+const headerNav = document.querySelector('.main-nav')
+const mapContainer = document.querySelector('.map__iframe-container')
 
 const openNav = () => {
-  headerToggle.classList.toggle("main-header__toggle--open")
-  headerNav.classList.toggle("main-nav--open")
+  headerToggle.classList.toggle('main-header__toggle--open')
+  headerNav.classList.toggle('main-nav--open')
 }
 
-headerToggle.addEventListener("click", openNav);
+headerToggle.addEventListener('click', openNav);
 
-mapContainer.classList.remove("map__iframe-container--no-js")
+mapContainer.classList.remove('map__iframe-container--no-js')
